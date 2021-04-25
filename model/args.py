4a4350fc93e76f21f902a4b556ba598a57b60631
Marco David <marco.david@spaceable.org>
2021-04-25 22:46:47 +0200

    Improve the training loop by choosing the model with the lowest test loss, and introduce minibatches with a configurable batch size.

--- model/args.py
+++ model/args.py
@@ -16,10 +16,10 @@ def get_args():
     # The dimension (of the input tensor) is now determined by the chosen system/data set via the 'name' arg
     parser.add_argument('--hidden_layers', default=1, type=int, help='number of hidden layers of the MLP')
     parser.add_argument('--hidden_dim', default=200, type=int, help='dimension of each hidden layer of the MLP')
-    parser.add_argument('--learn_rate', default=1e-3, type=float, help='learning rate')
     parser.add_argument('--nonlinearity', default='tanh', type=str, help='neural net nonlinearity')
-    parser.add_argument('--total_steps', default=1000, type=int, help='number of gradient steps')
-    parser.add_argument('--print_every', default=200, type=int, help='number of gradient steps between prints')
+    parser.add_argument('--learn_rate', default=1e-3, type=float, help='learning rate')
+    parser.add_argument('--epochs', default=1000, type=int, help='number of gradient steps')
+    parser.add_argument('--batch_size', default=500, type=int, help='number of data points used together in one batch')
 
     # SCIENTIFIC ARGUMENTS (all optional)
     parser.add_argument('--loss_type', default='midpoint', type=str,
@@ -36,5 +36,6 @@ def get_args():
     parser.add_argument('--seed', default=int(time.time()), type=int, help='random seed')
     parser.add_argument('--save_dir', default=None, type=str, help='where to save the trained model')
     parser.add_argument('--verbose', dest='verbose', action='store_true', help='verbose?')
+    parser.add_argument('--print_every', default=200, type=int, help='number of gradient steps between prints')
 
     return parser.parse_args()
