Add functionality to correct Hamiltonian after training with its partial derivatives – for now only supports corrections for the EulerSymplectic scheme.

model/hnn.py
@@ -8,7 +8,8 @@
 import torch
 
 from model.standard_nn import MLP
-from utils import symplectic_form, save_path
+from model.data import symplectic_form
+from utils import save_path
 
 
 def get_hnn(args):
@@ -34,6 +35,7 @@ def load_model(args):
 
 class HNN(torch.nn.Module):
     """ Learn arbitrary Hamiltonian vector fields that are the symplectic derivative of a scalar function H """
+
     def __init__(self, differentiable_model):
         super().__init__()
         self.differentiable_model = differentiable_model
@@ -56,3 +58,21 @@ class HNN(torch.nn.Module):
         # An alternative string for this batch operation would be 'ij,...j->...i' to not specify the extra dims.
 
         return vector_field
+
+
+class CorrectedHNN:
+    """ Use an HNN but correct the learned modified Hamiltonian to actually
+        and accurately predict the real Hamiltonian. """
+
+    def __init__(self, hnn, scheme, h):
+        super().__init__()
+        self.hnn = hnn
+        self.scheme = scheme
+        self.h = h
+
+    def corrected_forward(self, x):
+        hamiltonian = self.hnn(x)
+        hamiltonian_corrected = self.scheme.corrected(hamiltonian, x, self.h)
+        return hamiltonian_corrected
+
+    __call__ = corrected_forward

model/loss.py
@@ -1,6 +1,8 @@
 # Symplectic Hamiltonian Neural Networks | 2021
 # Marco David
 
+import warnings
+
 import torch
 import numpy as np
 from abc import ABC, abstractmethod
@@ -26,17 +28,42 @@ class ForwardEuler(OneStepScheme):
         return yn
 
 
-class SymplecticEuler(OneStepScheme):
+class SymplecticOneStepScheme(OneStepScheme, ABC):
+    @abstractmethod
+    def corrected(self, hamiltonian, x, h, order):
+        pass
+
+
+class SymplecticEuler(SymplecticOneStepScheme):
     def argument(self, yn, ynplusone):
         pn, qn = torch.split(yn, self.args.dim // 2, dim=-1)
         pnplusone, qnplusone = torch.split(ynplusone, self.args.dim // 2, dim=-1)
 
         return torch.cat((pnplusone, qn), dim=-1)
 
+    def corrected(self, hamiltonian, x, h, order=1):
+        if order > 1:
+            raise NotImplementedError("Higher order corrections are not (yet) implemented.")
 
-class ImplicitMidpoint(OneStepScheme):
+        dH = torch.autograd.grad(hamiltonian.sum(), x)[0]
+        dH_p, dH_q = torch.split(dH, self.args.dim // 2, dim=-1)
+
+        # Calculate the first-order correction to the Hamiltonian
+        correction = hamiltonian - h/2 * torch.dot(dH_p, dH_q)
+        return correction
+
+
+class ImplicitMidpoint(SymplecticOneStepScheme):
     def argument(self, yn, ynplusone):
         return (yn + ynplusone) / 2
+
+    def corrected(self, hamiltonian, x, h, order=1):
+        if order > 1:
+            raise NotImplementedError("Higher order corrections are not (yet) implemented.")
+
+        warnings.warn("The midpoint rule has no first order correction. Calling its `corrected` method is not yet"
+                      "supported and simply is the identity function for now.", RuntimeWarning)
+        return hamiltonian
 
 
 def choose_scheme(name):

utils.py
@@ -10,6 +10,8 @@
 import os
 import sys
 import pickle
+import numpy as np
+import torch
 from torch.nn import functional
 # import zipfile
 # import imageio
@@ -17,7 +19,7 @@ from torch.nn import functional
 # from PIL import Image
 
 from model.args import get_args
-from model.data import *
+from model.data import HarmonicOscillator, NonlinearPendulum, FermiPastaUlam
 
 
 # This function is generic, but needs to run in a top-level file to setup the path variables
@@ -121,6 +123,7 @@ def choose_nonlinearity(name):
     return choose_helper(nonlinearities, name, choose_what="Nonlinearity")
 
 
+# This function cannot be moved inside data.py because that would cause a circular import!
 def choose_data(name):
     datasets = {'spring': HarmonicOscillator,
                 'pendulum': NonlinearPendulum,